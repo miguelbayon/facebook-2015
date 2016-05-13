83816ecab85f076ad1550866dd29a4ce33f5a848
Miguel Bayón <publi.michelbayon@gmail.com>
2016-05-13 12:46:35 +0200

    Refactorizado el codigo introduciendo la clase Post

--- codigo.java
+++ codigo.java
@@ -58,42 +58,25 @@ public class NewsFeed
 
 
 
-
-
-
-
-
 /***********************************
-          MessagePost.java
+              Post.java
  ***********************************/
-
 import java.util.ArrayList;
-/**
- * Write a description of class MessagePost here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class MessagePost
+
+public class Post
 {
-    private String username;
-    private String message;
-    private long timestamp;
-    private int likes;
-    private ArrayList<String> comments;
+	private String username;
+  private long timestamp;
+  private int likes;
+  private ArrayList<String> comments;
 
-    /**
-     * Constructor for objects of class MessagePost
-     */
-    public MessagePost(String author, String text)
-    {
-       this.username = author;
-       this.message = text;
-       this.timestamp = System.currentTimeMillis();
-       likes = 0;
-       comments = new ArrayList<>();
-    }
-
+  public Post(String user){
+  	username = user;
+    timestamp = System.currentTimeMillis();
+  	comments = new ArrayList<>();
+    likes = 0;
+  }
+  
     /**
      * Metodo para dar un like.
      */
@@ -115,13 +98,7 @@ public class MessagePost
     public void addComment(String text){
         comments.add(text);
     }
-    
-    /**
-     * Metodo que devuelve el mensaje del post
-     */
-    public String getText(){
-        return message;
-    }
+  
     
     /**
      * Metodo que devuelve la estampa de tiempo en el momento de crear el post
@@ -138,7 +115,6 @@ public class MessagePost
         long time = System.currentTimeMillis() - getTimeStamp();
         info += username + "\n=====================\n" + "Posted: ";
         info += timeString(time);
-        info += "=====================\n" + getText() + "\n";
         info += "_____________________\nLikes: " + likes + "\n=====================\n\n";
         if(comments.size() != 0)
             for(int i=0; i<comments.size(); i++)
@@ -159,6 +135,44 @@ public class MessagePost
             info += min + " Minutes, ";
         info += sec + " Seconds\n";
         return info;
+    }  
+  
+  
+}
+
+
+
+
+
+/***********************************
+          MessagePost.java
+ ***********************************/
+
+import java.util.ArrayList;
+/**
+ * Write a description of class MessagePost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MessagePost
+{
+    private String message;
+
+    /**
+     * Constructor for objects of class MessagePost
+     */
+    public MessagePost(String author, String text)
+    {
+      super(author);
+      message = text;
+    }
+    
+    /**
+     * Metodo que devuelve el mensaje del post
+     */
+    public String getText(){
+        return message;
     }
 }
 
@@ -180,48 +194,19 @@ import java.util.ArrayList;
  */
 public class PhotoPost
 {
-    private String username;
     private String filename;
     private String caption;
-    private long timestamp;
-    private int likes;
-    private ArrayList<String> comments;
 
     /**
      * Constructor for objects of class MessagePost
      */
     public PhotoPost(String author, String filename, String caption)
     {
-       this.username = author;
-       this.filename = filename;
-       this.timestamp = System.currentTimeMillis();
-       this.caption = caption;
-       likes = 0;
-       comments = new ArrayList<>();
+      super(author);
+      this.filename = filename;
+      this.caption = caption;
     }
 
-    /**
-     * Metodo para dar un like.
-     */
-    public void like(){
-        likes++;
-    }
-    
-    /**
-     * Metodo para quitar un like en caso de que los haya.
-     */
-    public void unlike(){
-        if(likes != 0)
-           likes--; 
-    }
-    
-    /**
-     * Metodo para añadir un comentario al post
-     */
-    public void addComment(String text){
-        comments.add(text);
-    }
-    
     /**
      * Meotdo que devuelve el nombre del archivo
      */
@@ -235,70 +220,4 @@ public class PhotoPost
     public String getCaption(){
         return caption;
     }
-    
-    /**
-     * Metodo que devuelve la estampa de tiempo en el momento de crear el post
-     */
-    public long getTimeStamp(){
-        return timestamp;
-    }
-    
-    /**
-     * Metodo que muestra toda la info del post
-     */
-    public void display(){
-        String info = "";
-        long time = System.currentTimeMillis() - getTimeStamp();
-        info += username + "\n=====================\n" + "Posted: ";
-        info += timeString(time);
-        info += getImageFile() + "\n";
-        info += getCaption() + "\n";
-        info += "_____________________\nLikes: " + likes + "\n=====================\n\n";
-        if(comments.size() != 0)
-            for(int i=0; i<comments.size(); i++)
-                info += comments.get(i) + "\n_____________________\n";
-        else
-            info += "Sin comentarios";
-        System.out.println(info);
-    }
-    
-    /**
-     * Metodo para pasar el tiempo a minutos y segundos.
-     */
-    private String timeString(long time){
-        String info = "";
-        int sec =  (int)(time / 1000) % 60;
-        int min =  (int)((time / (1000*60)) % 60);
-        if(min > 0)
-            info += min + " Minutes, ";
-        info += sec + " Seconds\n";
-        return info;
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
